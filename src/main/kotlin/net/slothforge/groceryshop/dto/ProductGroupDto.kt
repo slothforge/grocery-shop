483dd0db83c75ad7459d3985b6e54d6aa5766b44
slothforge <evlgorbachev@gmail.com>
2018-02-10 21:26:52 +0300

    Remove MyBatis mappers, services and models.

--- src/main/kotlin/net/slothforge/groceryshop/dto/ProductGroupDto.kt
+++
@@ -1,9 +0,0 @@
-package net.slothforge.groceryshop.dto
-
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class ProductGroupDto @JsonCreator constructor(
-        @JsonProperty val name: String,
-        @JsonProperty val description: String
-)
--- src/main/kotlin/net/slothforge/groceryshop/entity/ProductGroup.kt
+++
@@ -1,7 +0,0 @@
-package net.slothforge.groceryshop.entity
-
-data class ProductGroup(
-        val id: Long,
-        val name: String,
-        val description: String
-)
